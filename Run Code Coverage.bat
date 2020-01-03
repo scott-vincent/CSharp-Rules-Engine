2c2abd583376eadd7cdc433873cfaad06f4413cb
scott-vincent <scottvincent7@gmail.com>
2020-01-03 08:38:09 +0000

    Add pre-reqs for code coverage

--- Run Code Coverage.bat
+++ Run Code Coverage.bat
@@ -1,4 +1,13 @@
 @echo off
+REM Pre-Requisites
+REM   Install NuGet package coverlet.msbuild and add to UnitTest projects
+REM   Install NuGet package ReportGenerator as a global tool:
+REM     dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.4.0
+REM
+REM You can run this script directly from Visual Studio if you wish:
+REM   Tools -> External Tools...
+REM     Title: Run Code Coverage, Command: cmd.exe, Arguments: /c "Run Code Coverage.bat", Initial dir: $(SolutionDir)  
+
 call :RunCoverage RulesEngine-UnitTests
 goto :eof
 
